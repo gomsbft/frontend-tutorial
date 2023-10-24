b08928fbedbf59b0ddbbdc0a6dd4e64b89481c24
한지원 <hanjiwon@hanjiwon-ui-MacBookAir.local>
2023-10-24 10:38:38 +0900

    6week homework

---
+++ 6week/homwork.js
@@ -0,0 +1,27 @@
+"use strich";
+
+let browser = prompt('브라우저를 입력해주세요', '');
+
+if (browser === 'Edge') {
+    alert('Edge를 사용하고 계시네요!');
+}
+
+else if (btorowser === 'Chrome') {
+    alert('저희 서비스가 지원하는 브라우저를 사용하고 계시네요.');
+}
+
+else if (browser === 'Firefox') {
+    alert('저희 서비스가 지원하는 브라우저를 사용하고 계시네요.');
+}
+
+else if (browser === 'Safari') {
+    alert('저희 서비스가 지원하는 브라우저를 사용하고 계시네요.');
+}
+
+else if (browser === 'Opera') {
+    alert('저희 서비스가 지원하는 브라우저를 사용하고 계시네요.');
+}
+
+else {
+    alert('현재 페이지가 괜찮아 보이길 바랍니다!');
+}
